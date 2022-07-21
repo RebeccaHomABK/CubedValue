dfeac8aaa50a8209092d133ca98c913a7cb6605b
RebeccaHomABK <109548240+RebeccaHomABK@users.noreply.github.com>
2022-07-21 13:16:20 -0700

    updated cubed value to use the power function from the math library

--- CubedValue/CubedValue.cpp
+++ CubedValue/CubedValue.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-int Power(int base, int exponent);
-int Cubed(int base);
+//int Power(int base, int exponent);
+//int Cubed(int base);
 
 int main()
 {
@@ -15,20 +16,22 @@ int main()
     cin >> number;
 
     //  Return the number cubed
-    cout << "Your cubed value is : " << /*Power(number, 3)*/ Cubed(number) << endl;
+    //cout << "Your cubed value is : " << Power(number, 3) << endl;
+    //cout << "Your cubed value is : " << Cubed(number) << endl;
+    cout << "Your cubed value is : " << pow(number, 3) << endl;
 
 }
 
-int Power(int base, int exponent)
-{
-    if (exponent <= 0) {
-        return 1;
-    }
-
-    return base * Power(base, exponent - 1);
-}
-
-int Cubed(int base)
-{
-    return base * base * base;
-}
+//int Power(int base, int exponent)
+//{
+//    if (exponent <= 0) {
+//        return 1;
+//    }
+//
+//    return base * Power(base, exponent - 1);
+//}
+//
+//int Cubed(int base)
+//{
+//    return base * base * base;
+//}
